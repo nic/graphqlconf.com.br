03b142a0c6f2105cfcaf7279fb2eb3a5866af84e
Nic <nic@contaquanto.com.br>
2019-03-06 05:02:10 -0300

    Updates

---
+++ precache-manifest.3cde861c7af8e49701154554fccfe1ff.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e8e750aa508298060c9a91fda8ca9b1",
+    "url": "/static/media/slack.0e8e750a.svg"
+  },
+  {
+    "revision": "74c373ab6dd0e5b344e8",
+    "url": "/static/css/main.23dc43fb.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "98e0a999a03c3a09f12a",
+    "url": "/static/js/2.98e0a999.chunk.js"
+  },
+  {
+    "revision": "f5b400ae35c779070cdece5b13c85599",
+    "url": "/static/media/Logo.f5b400ae.png"
+  },
+  {
+    "revision": "efdcf23e484a1a1a5954adcf256a1a1c",
+    "url": "/static/media/atomo.efdcf23e.svg"
+  },
+  {
+    "revision": "cb57dd6eabda6645adca2de67a8eb069",
+    "url": "/static/media/facebook.cb57dd6e.svg"
+  },
+  {
+    "revision": "f0b496725076a33da8a147bce6381156",
+    "url": "/static/media/twitter.f0b49672.svg"
+  },
+  {
+    "revision": "74c373ab6dd0e5b344e8",
+    "url": "/static/js/main.74c373ab.chunk.js"
+  },
+  {
+    "revision": "5d85746f8dde471b89ee50e146b18178",
+    "url": "/static/media/email.5d85746f.svg"
+  },
+  {
+    "revision": "b8204f6c8ec99df6bb3393c0432395a8",
+    "url": "/static/media/GraphQL.b8204f6c.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "98e0a999a03c3a09f12a",
+    "url": "/static/css/2.6b3ba57e.chunk.css"
+  },
+  {
+    "revision": "c53fe0531ce80efcc7b74b6f81909719",
+    "url": "/index.html"
+  }
+];
