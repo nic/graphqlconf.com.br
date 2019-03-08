2b8de9ce98991dc13eb7843d3467efd6b45d2152
Leticia Godoy <let.godoy@live.com>
2019-03-07 23:34:54 -0300

    acentuação

--- src/components/template/Ingressos.jsx
+++ src/components/template/Ingressos.jsx
@@ -22,7 +22,7 @@ class Ingresso extends React.Component {
           {/* <div class="card-header">Ingressos</div> */}
           <div className="card-body">
             <h5 className="card-title">Ingressos</h5>
-            <p className="card-text">Em breve os ingressos estarão disponiveis para compra.</p>
+            <p className="card-text">Em breve os ingressos estarão disponíveis para compra.</p>
           </div>
         </div>
       </a>
